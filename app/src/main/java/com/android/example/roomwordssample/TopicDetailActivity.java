4170150c972da9aa34559aa41fccd57d334f5a9f
shfuentes26 <shfuentes@gmail.com>
2021-12-29 12:05:07 -0600

    Adding new task files

--- app/src/main/java/com/android/example/roomwordssample/TopicDetailActivity.java
+++ app/src/main/java/com/android/example/roomwordssample/TopicDetailActivity.java
@@ -6,19 +6,30 @@ import static com.android.example.roomwordssample.MainActivity.EXTRA_DATA_DESC;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class TopicDetailActivity extends AppCompatActivity {
 
+    public static final String EXTRA_REPLY = "com.example.android.roomwordssample.REPLY";
+    public static final String EXTRA_REPLY_DESC = "com.example.android.roomwordssample.REPLY_DESC";
+    public static final String EXTRA_REPLY_ID = "com.android.example.roomwordssample.REPLY_ID";
+    public static final int CREATE_TASK_ACTIVITY_REQUEST_CODE = 1;
+
     private TextView mTopicNameTxt;
     private TextView mTopicDesTxt;
     private TopicViewModel mTopicViewModel;
+    int id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +39,11 @@ public class TopicDetailActivity extends AppCompatActivity {
         mTopicNameTxt = findViewById(R.id.topic_name_txt);
         mTopicDesTxt = findViewById(R.id.topic_desc_txt);
 
+
         final Bundle extras = getIntent().getExtras();
         // If we are passed content, fill it in for the user to edit.
         if (extras != null) {
-            Integer id = extras.getInt(EXTRA_DATA_ID);
+            id = extras.getInt(EXTRA_DATA_ID);
             String description = extras.getString(EXTRA_DATA_DESC);
             String topic = extras.getString(EXTRA_DATA_UPDATE_TOPIC, "");
             if (!topic.isEmpty()) {
@@ -39,8 +51,15 @@ public class TopicDetailActivity extends AppCompatActivity {
                 mTopicDesTxt.setText(description);
             }
         }
-
-
+        //button to create a task for the topic, it redirects to the NewTaskActivity
+        final Button button = findViewById(R.id.button_add_task);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(TopicDetailActivity.this,
+                       "Creating new tasks...", Toast.LENGTH_LONG).show();
+                launchCreateTaskActivity(id);
+            }
+        });
 
 
         // Set up the WordViewModel.
@@ -57,5 +76,11 @@ public class TopicDetailActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void launchCreateTaskActivity( int topicId) {
+        Intent intent = new Intent(this, NewTaskActivity.class);
+        intent.putExtra(EXTRA_DATA_UPDATE_TOPIC, topicId);
+        startActivityForResult(intent, CREATE_TASK_ACTIVITY_REQUEST_CODE);
     }
 }
